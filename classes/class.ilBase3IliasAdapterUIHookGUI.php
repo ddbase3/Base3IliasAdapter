52dd4d51bf8b30b2af1510231a01eb9c70daefad
qualitus-dahme <Dahme@qualitus.de>
2026-03-22 12:26:15 +0100

    cleanup

--- classes/class.ilBase3IliasAdapterUIHookGUI.php
+++ classes/class.ilBase3IliasAdapterUIHookGUI.php
@@ -13,97 +13,91 @@ use Base3\Hook\IHookListener;
 use Base3\Hook\IHookManager;
 use Base3\ServiceSelector\Api\IServiceSelector;
 use Base3\ServiceSelector\Standard\StandardServiceSelector;
-use Base3Ilias\Api\IPageComponentConfigStore;
 use Base3Ilias\Base3\Base3IliasClassMap;
 use Base3Ilias\Base3\Base3IliasServiceLocator;
 use Base3Ilias\Base3\Base3IliasSystemService;
 use Base3Ilias\External\IliasPsrContainer;
-use Base3Ilias\Service\PageComponentConfigStore;
 use ILIAS\DI\Container;
 
 class ilBase3IliasAdapterUIHookGUI extends ilUIHookPluginGUI {
-
-    public function __construct() {
-        $this->base3IliasBootstrap();
-    }
-
-    public function getHTML(string $a_comp, string $a_part, array $a_par = array()): array {
-        return ['mode' => ilUIHookPluginGUI::KEEP, 'html' => ''];
-    }
-
-    // Private methods
-
-    private function base3IliasBootstrap() {
-
-        // ILIAS base dir
-        if (!defined('DIR_ILIAS')) define('DIR_ILIAS', realpath(__DIR__ . '/../../../../../../../../..') . DIRECTORY_SEPARATOR);
-
-	// Generate data dir and tmp dir from ini (Remember to create DIR_TMP!)
-	$iliasConfig = parse_ini_file(DIR_ILIAS . 'ilias.ini.php', true);
-	$dataDir = $clientDir = $tmpDir = '';
-	if (isset($iliasConfig['clients']) && isset($iliasConfig['clients']['datadir']) && isset($iliasConfig['clients']['default'])) {
-	        $dataDir = $iliasConfig['clients']['datadir'] . '/';
-	        $clientDir = $dataDir . $iliasConfig['clients']['default'] . '/';
+	public function __construct() {
+		$this->base3IliasBootstrap();
 	}
-	if (!defined('DIR_DATA')) define('DIR_DATA', $dataDir);
-	if (!defined('DIR_CLIENT')) define('DIR_CLIENT', $clientDir);
 
-	// installation dirs
-	if (!defined('DIR_COMPONENTS')) define('DIR_COMPONENTS', DIR_ILIAS . 'components/');
-	if (!defined('DIR_BASE3')) define('DIR_BASE3', DIR_COMPONENTS . 'Base3/');
-	if (!defined('DIR_FRAMEWORK')) define('DIR_FRAMEWORK', DIR_BASE3 . 'Base3Framework/');
-	if (!defined('DIR_SRC')) define('DIR_SRC', DIR_FRAMEWORK . 'src/');
-	if (!defined('DIR_TEST')) define('DIR_TEST', DIR_FRAMEWORK . 'test/');
-	if (!defined('DIR_PLUGIN')) define('DIR_PLUGIN', DIR_BASE3);
-	if (!defined('DIR_TMP')) define('DIR_TMP', DIR_BASE3 . 'tmp/');
-	if (!defined('DIR_LOCAL')) define('DIR_LOCAL', DIR_TMP);
+	public function getHTML(string $a_comp, string $a_part, array $a_par = array()): array {
+		return ['mode' => ilUIHookPluginGUI::KEEP, 'html' => ''];
+	}
 
+	// Private methods
 
-        // Debug mode - 0: aus, 1: an, ggfs noch höhere Stufen?
-        putenv('DEBUG=1');
+	private function base3IliasBootstrap() {
+		// ILIAS base directory
+		if (!defined('DIR_ILIAS')) define('DIR_ILIAS', realpath(__DIR__ . '/../../../../../../../../..') . DIRECTORY_SEPARATOR);
 
-        // error handling
-        ini_set('display_errors', getenv('DEBUG') ? 1 : 0);
-        ini_set('display_startup_errors', getenv('DEBUG') ? 1 : 0);
-        error_reporting(getenv('DEBUG') ? E_ALL | E_STRICT : 0);
+		// Generate data directory and temp directory from ini (remember to create DIR_TMP!)
+		$iliasConfig = parse_ini_file(DIR_ILIAS . 'ilias.ini.php', true);
+		$dataDir = $clientDir = $tmpDir = '';
+		if (isset($iliasConfig['clients']) && isset($iliasConfig['clients']['datadir']) && isset($iliasConfig['clients']['default'])) {
+			$dataDir = $iliasConfig['clients']['datadir'] . '/';
+			$clientDir = $dataDir . $iliasConfig['clients']['default'] . '/';
+		}
+		if (!defined('DIR_DATA')) define('DIR_DATA', $dataDir);
+		if (!defined('DIR_CLIENT')) define('DIR_CLIENT', $clientDir);
 
-        // service locator
-        $servicelocator = new Base3IliasServiceLocator();
-        ServiceLocator::useInstance($servicelocator);
-        $servicelocator
-            ->set('servicelocator', $servicelocator, IContainer::SHARED)
-            ->set(ISystemService::class, fn() => new Base3IliasSystemService(), IContainer::SHARED)
-	    ->set(IRequest::class, Request::fromGlobals(), IContainer::SHARED)
-            ->set(IContainer::class, 'servicelocator', IContainer::ALIAS)
-            ->set(IHookManager::class, fn() => new HookManager, ServiceLocator::SHARED)
-            ->set('classmap', new Base3IliasClassMap($servicelocator), IContainer::SHARED)
-            ->set(IClassMap::class, 'classmap', IContainer::ALIAS)
-            ->set(IServiceSelector::class, fn() => new StandardServiceSelector($servicelocator), IContainer::SHARED)
-	    ->set(IPageComponentConfigStore::class, fn($c) => new PageComponentConfigStore($c->get(IDatabase::class)), IContainer::SHARED);
+		// Installation directories
+		if (!defined('DIR_COMPONENTS')) define('DIR_COMPONENTS', DIR_ILIAS . 'components/');
+		if (!defined('DIR_BASE3')) define('DIR_BASE3', DIR_COMPONENTS . 'Base3/');
+		if (!defined('DIR_FRAMEWORK')) define('DIR_FRAMEWORK', DIR_BASE3 . 'Base3Framework/');
+		if (!defined('DIR_SRC')) define('DIR_SRC', DIR_FRAMEWORK . 'src/');
+		if (!defined('DIR_TEST')) define('DIR_TEST', DIR_FRAMEWORK . 'test/');
+		if (!defined('DIR_PLUGIN')) define('DIR_PLUGIN', DIR_BASE3);
+		if (!defined('DIR_TMP')) define('DIR_TMP', DIR_BASE3 . 'tmp/');
+		if (!defined('DIR_LOCAL')) define('DIR_LOCAL', DIR_TMP);
 
-        // fill container with ILIAS services
-        $servicelocator->setIliasContainer(new IliasPsrContainer($GLOBALS['DIC']));
-        $servicelocator->set(\ILIAS\DI\Container::class, $GLOBALS['DIC'], IContainer::SHARED);
+		// Debug mode - 0: off, 1: on, possibly higher levels?
+		putenv('DEBUG=1');
 
-        // hooks
-        $hookManager = $servicelocator->get(IHookManager::class);
-        $listeners = $servicelocator->get(IClassMap::class)->getInstancesByInterface(IHookListener::class);
-        foreach ($listeners as $listener) $hookManager->addHookListener($listener);
-        $hookManager->dispatch('bootstrap.init');
+		// Error handling
+		ini_set('display_errors', getenv('DEBUG') ? 1 : 0);
+		ini_set('display_startup_errors', getenv('DEBUG') ? 1 : 0);
+		error_reporting(getenv('DEBUG') ? E_ALL | E_STRICT : 0);
 
-        // plugins
-        $plugins = $servicelocator->get(IClassMap::class)->getInstancesByInterface(IPlugin::class);
-        foreach ($plugins as $plugin) $plugin->init();
-        $hookManager->dispatch('bootstrap.start');
+		// Service locator
+		$servicelocator = new Base3IliasServiceLocator();
+		ServiceLocator::useInstance($servicelocator);
+		$servicelocator
+			->set('servicelocator', $servicelocator, IContainer::SHARED)
+			->set(ISystemService::class, fn() => new Base3IliasSystemService(), IContainer::SHARED)
+			->set(IRequest::class, Request::fromGlobals(), IContainer::SHARED)
+			->set(IContainer::class, 'servicelocator', IContainer::ALIAS)
+			->set(IHookManager::class, fn() => new HookManager, ServiceLocator::SHARED)
+			->set('classmap', new Base3IliasClassMap($servicelocator), IContainer::SHARED)
+			->set(IClassMap::class, 'classmap', IContainer::ALIAS)
+			->set(IServiceSelector::class, fn() => new StandardServiceSelector($servicelocator), IContainer::SHARED);
 
-        // fill ilias container with base3 services
-        $services = $servicelocator->getServiceList();
-        foreach ($services as $service) {
-            if (isset($GLOBALS['DIC'][$service])) continue;
-            $GLOBALS['DIC'][$service] = $servicelocator->get($service);
-        }
+		// Fill container with ILIAS services
+		$servicelocator->setIliasContainer(new IliasPsrContainer($GLOBALS['DIC']));
+		$servicelocator->set(\ILIAS\DI\Container::class, $GLOBALS['DIC'], IContainer::SHARED);
 
-        // go
-        $hookManager->dispatch('bootstrap.finish');
-    }
+		// Hooks
+		$hookManager = $servicelocator->get(IHookManager::class);
+		$listeners = $servicelocator->get(IClassMap::class)->getInstancesByInterface(IHookListener::class);
+		foreach ($listeners as $listener) $hookManager->addHookListener($listener);
+		$hookManager->dispatch('bootstrap.init');
+
+		// Plugins
+		$plugins = $servicelocator->get(IClassMap::class)->getInstancesByInterface(IPlugin::class);
+		foreach ($plugins as $plugin) $plugin->init();
+		$hookManager->dispatch('bootstrap.start');
+
+		// Fill ILIAS container with Base3 services
+		$services = $servicelocator->getServiceList();
+		foreach ($services as $service) {
+			if (isset($GLOBALS['DIC'][$service])) continue;
+			$GLOBALS['DIC'][$service] = $servicelocator->get($service);
+		}
+
+		// Go
+		$hookManager->dispatch('bootstrap.finish');
+	}
 }
