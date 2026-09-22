3013b0c30076af19ca9d2d1baaa8ed912121b058
qualitus-dahme <Dahme@qualitus.de>
2026-09-22 08:33:59 +0200

    feat: lib dir for installation freedom

--- plugin.php
+++ plugin.php
@@ -2,7 +2,7 @@
 
 $id = 'base3iliasadapter';
 $version = '4.8.0';
-$ilias_min_version = '10.0';
+$ilias_min_version = '9.0';
 $ilias_max_version = '12.999';
 $responsible = 'Daniel Dahme';
 $responsible_mail = 'info@base3.de';
